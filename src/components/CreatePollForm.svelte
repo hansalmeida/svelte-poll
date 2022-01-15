fcc4eb0d77d52069ce1215687b2f0e2a8d749411
hansalmeida <hansalmeida@hotmail.com>
2022-01-15 18:07:50 -0300

    Add Create Poll form submit handler

--- src/components/CreatePollForm.svelte
+++ src/components/CreatePollForm.svelte
@@ -6,9 +6,40 @@
     answer1: "",
     answer2: "",
   }
+  const errors = {
+    question: "",
+    answer1: "",
+    answer2: "",
+  }
+  let valid = false
 
   const handleSubmit = () => {
-    console.log(fields)
+    valid = true
+
+    if (fields.question.trim().length < 5) {
+      valid = false
+      errors.question = "Question must have a minimum length of 5"
+    } else {
+      errors.question = ""
+    }
+
+    if (fields.answer1.trim().length < 1) {
+      valid = false
+      errors.answer1 = "First answer cannot be empty"
+    } else {
+      errors.answer1 = ""
+    }
+
+    if (fields.answer2.trim().length < 1) {
+      valid = false
+      errors.answer2 = "Second answer cannot be empty"
+    } else {
+      errors.answer2 = ""
+    }
+
+    if (valid) {
+      console.log("valid", fields)
+    }
   }
 </script>
 
@@ -16,16 +47,19 @@
   <div class="form-field">
     <label for="question">Poll question:</label>
     <input type="text" id="question" bind:value={fields.question} />
+    <div class="error">{errors.question}</div>
   </div>
 
   <div class="form-field">
     <label for="answer-1">Answer 1:</label>
     <input type="text" id="answer-1" bind:value={fields.answer1} />
+    <div class="error">{errors.answer1}</div>
   </div>
 
   <div class="form-field">
     <label for="answer-2">Answer 2:</label>
     <input type="text" id="answer-2" bind:value={fields.answer2} />
+    <div class="error">{errors.answer2}</div>
   </div>
 
   <Button type="secondary" fit>Add poll</Button>
@@ -54,5 +88,10 @@
 
       border-radius: 6px;
     }
+  }
+  .error {
+    color: #d91b42;
+    font-weight: bold;
+    font-size: 12px;
   }
 </style>
