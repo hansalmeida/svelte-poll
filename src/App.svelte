1e0098dd80839e8d695933697c58643729cbc760
hansalmeida <hansalmeida@hotmail.com>
2022-01-15 17:26:40 -0300

    Create Poll form component layout

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import CreatePollForm from "./components/CreatePollForm.svelte"
+
   import Footer from "./components/Footer.svelte"
   import Header from "./components/Header.svelte"
   import Tabs from "./shared/Tabs.svelte"
@@ -16,7 +18,7 @@
     <h2>Current Pools</h2>
   {/if}
   {#if activeTab === "Add New Poll"}
-    <h2>Add new Poll</h2>
+    <CreatePollForm />
   {/if}
 </main>
 <Footer />
---
+++ src/components/CreatePollForm.svelte
@@ -0,0 +1,51 @@
+<script lang="ts">
+  const fields = {
+    question: "",
+    answer1: "",
+    answer2: "",
+  }
+
+  const handleSubmit = () => {
+    console.log(fields)
+  }
+</script>
+
+<form on:submit|preventDefault={handleSubmit}>
+  <div class="form-field">
+    <label for="question">Poll question:</label>
+    <input type="text" id="question" bind:value={fields.question} />
+  </div>
+  <div class="form-field">
+    <label for="answer-1">Answer 1:</label>
+    <input type="text" id="answer-1" bind:value={fields.answer1} />
+  </div>
+  <div class="form-field">
+    <label for="answer-2">Answer 2:</label>
+    <input type="text" id="answer-2" bind:value={fields.answer2} />
+  </div>
+  <button>Add poll</button>
+</form>
+
+<style lang="scss">
+  form {
+    display: grid;
+    gap: 18px;
+
+    width: 400px;
+    margin: 0 auto;
+
+    text-align: center;
+  }
+  .form-field {
+    label {
+      margin: 10px auto;
+
+      text-align: left;
+    }
+    input {
+      width: 100%;
+
+      border-radius: 6px;
+    }
+  }
+</style>
