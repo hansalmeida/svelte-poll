3ff5b67357b064853dcd7a645bd8f014a8a80548
hansalmeida <hansalmeida@hotmail.com>
2022-01-16 06:32:38 -0300

    Add poll list transtitions and animations

--- src/components/PollList.svelte
+++ src/components/PollList.svelte
@@ -1,4 +1,6 @@
 <script lang="ts">
+  import { flip } from "svelte/animate"
+  import { fade, scale } from "svelte/transition"
   import { PollStore } from "../stores/PollStore"
   import PollDetails from "./PollDetails.svelte"
 
@@ -10,7 +12,9 @@
 
 <div class="poll-list">
   {#each $PollStore as poll (poll.id)}
-    <PollDetails {poll} />
+    <div in:fade out:scale|local animate:flip={{ duration: 500 }}>
+      <PollDetails {poll} />
+    </div>
   {/each}
 </div>
 
