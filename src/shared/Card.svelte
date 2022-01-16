ef23bbfe6cf8ec2e89d7c219668672c8eccff3f1
hansalmeida <hansalmeida@hotmail.com>
2022-01-16 05:36:09 -0300

    Create Card component

---
+++ src/shared/Card.svelte
@@ -0,0 +1,13 @@
+<div class="card">
+  <slot />
+</div>
+
+<style lang="scss">
+  .card {
+    padding: 20px;
+
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+    border-radius: 6px;
+    background: white;
+  }
+</style>
